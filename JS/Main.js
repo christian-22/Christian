bbfd8bb1dd4f90e208104e27e1c4c7aa13c2d2d9
christian-22 <71905486+christian-22@users.noreply.github.com>
2023-07-11 13:09:24 -0500

    Add files via upload
    loading animation functionality

---
+++ JS/Main.js
@@ -0,0 +1,14 @@
+const body = document.body;
+const lastPanel = document.querySelector(".panels .panel:last-child");
+
+window.addEventListener("load", () => {
+  body.classList.add("loaded");
+  
+  lastPanel.addEventListener("transitionend", () => {
+    if (body.classList.contains("second-round")) {
+      body.classList.add("third-round");
+    } else {
+      body.classList.add("second-round");
+    }
+  });
+});
