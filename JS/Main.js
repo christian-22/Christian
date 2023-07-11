6b3060236a1758cb55237180e1a2c400125bb34e
christian-22 <71905486+christian-22@users.noreply.github.com>
2023-07-11 15:58:01 -0500

    Update Main.js
    loading bar

--- JS/Main.js
+++ JS/Main.js
@@ -1,8 +1,32 @@
+// loading bar
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("loading-bar");
+    var width = 5;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
+
+
+// loading transition animation
 const body = document.body;
 const lastPanel = document.querySelector(".panels .panel:last-child");
 
 window.addEventListener("load", () => {
   body.classList.add("loaded");
+  move();
   
   lastPanel.addEventListener("transitionend", () => {
     if (body.classList.contains("second-round")) {
